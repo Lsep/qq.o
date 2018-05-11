76e7e9bfb06cd80a4e9146f8abebc15391f9595a
qq <867919505@qq.com>
2018-05-11 15:28:42 +0800

    创建

---
+++ 30.o.web/src/main/java/qq/web/controller/TestController.java
@@ -0,0 +1,21 @@
+package qq.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author qq
+ * @date 2018/5/11
+ */
+@Controller("test-controller")
+@RequestMapping("/test")
+public class TestController extends _ControllerBase {
+
+    @GetMapping("/index")
+    public ModelAndView index(){
+        return super.tryView("test-index",null);
+    }
+
+}
